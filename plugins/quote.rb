0f1989c94f8f05ed420752330442e9e53fb47042
Sean O'Dowd <sean.odowd@gmail.com>
2008-08-07 23:56:26 +1000

    Added quotes plug-in to display quotes from the Web in campfire

---
+++ plugins/quote.rb
@@ -0,0 +1,31 @@
+require 'open-uri'
+require 'hpricot'
+
+class Quote < PluginBase
+  on_command 'quote', :quote
+  
+  def quote(msg)
+    # Get our quotes from the web
+    url = "http://quotes4all.net/rss/000010110/quotes..xml"
+    response = ''
+
+    begin
+      # open-uri RDoc: http://stdlib.rubyonrails.org/libdoc/open-uri/rdoc/index.html
+      open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}",
+        "From" => "Campfire") { |f|
+                                  
+        # Save the response body
+        response = f.read
+      }
+    
+      # HPricot RDoc: http://code.whytheluckystiff.net/hpricot/
+      doc = Hpricot(response)
+
+      speak((doc/"*/item/description").inner_html.gsub(/<\/?[^>]*>/,"").gsub(/\s+/," ").gsub(/\&quote;/,"'").gsub(/\&[\#|\w]\w+\;/,"").gsub(/\#39\;/,"'"))
+      speak((doc/"*/item/title").inner_html)
+
+    rescue Exception => e
+      speak(e, "\n")
+    end
+  end
+end
