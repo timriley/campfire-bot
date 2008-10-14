2868cd1b9e03ffe6815cc50b392e32ef443ad3ec
Tim Riley <tim@openmonkey.com>
2008-10-14 21:45:03 +1100

    simplify and fix bugs in the twitter_echo plugin: keep the latest tweet date in memory instead of writing to a file, and also fetch the raw feed from search.twitter.com with every time the echo_tweets plugin is called so that any new tweets can actually get printed

--- plugins/twitter_echo.rb
+++ plugins/twitter_echo.rb
@@ -1,11 +1,10 @@
-require 'rio'
 require 'hpricot'
+require 'open-uri'
 require 'ostruct'
 require 'time'
 require 'htmlentities'
-require 'fileutils'
 
-# The workings of this plugin come directly from http://github.com/paulca/twitter2campfire
+# The workings of this plugin are based on http://github.com/paulca/twitter2campfire
 # Thanks to Paul Campbell and Contrast! <http://www.contrast.ie/>
 
 class TwitterEcho < PluginBase
@@ -13,56 +12,42 @@ class TwitterEcho < PluginBase
   at_interval 2.minutes, :echo_tweets
   
   def initialize
-    @cache  = "#{BOT_ROOT}/tmp/twitter_echo_cache.#{BOT_ENVIRONMENT}.txt"
     @feed   = Bot.instance.config['twitter_feed']
-
-    # make the dir for the cache file
-    FileUtils.mkdir_p(File.join(BOT_ROOT, 'tmp'))
-    # and touch the file
-    FileUtils.touch(@cache)
+    @latest = Time.now
   end
   
-  def echo_tweets
-    posts.reverse.each do |post|
-      speak("#{coder.decode(post.from)}: #{coder.decode(post.text)} #{post.link}")
+  def echo_tweets(msg = nil)
+    recent_tweets.reverse.each do |tweet|
+      speak("#{coder.decode(tweet.from)}: #{coder.decode(tweet.text)} #{tweet.link}")
     end
-    save_latest
+    @latest = latest_tweet.date   # next time, only print tweets newer than this
+    @doc    = nil                 # reset the feed so that next time we can actually any new tweets
   end
   
   protected
   
   def raw_feed
-    @doc ||= Hpricot(rio(@feed) > (string ||= ""))
+    @doc ||= Hpricot(open(@feed))
   end
   
-  def entries
-    (raw_feed/'entry').map { |e| OpenStruct.new(:from => (e/'name').inner_html, :text => (e/'title').inner_html, :link => (e/'link').first['href'], :date => Time.parse((e/'published').inner_html)) }
+  def all_tweets
+    (raw_feed/'entry').map { |e| OpenStruct.new(
+      :from => (e/'name').inner_html,
+      :text => (e/'title').inner_html,
+      :link => (e/'link').first['href'],
+      :date => Time.parse((e/'published').inner_html)
+    )}
   end
   
   def latest_tweet
-    entries.first
+    all_tweets.first
   end
-  
-  def save_latest
-    archive_file.truncate
-    archive_file << latest_tweet.date.to_s
-  end
-  
-  def archive_file
-    rio(@cache)
-  end
-  
-  def archived_latest_date
-    archive_file >> (string ||= "")
-    Time.parse(string)
-  end
-  
-  def posts
-    entries.reject { |e| e.date.to_i <= archived_latest_date.to_i }
+
+  def recent_tweets
+    all_tweets.reject { |e| e.date.to_i <= @latest.to_i }
   end
   
   def coder
     HTMLEntities.new
   end
 end
-
