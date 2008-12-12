0da5614c8bd2d6ece478c3ea7a22952a7799e490
Tim Riley <tim@openmonkey.com>
2008-12-12 11:53:38 +1100

    fix misnamed variable in figlet plugin

--- plugins/figlet.rb
+++ plugins/figlet.rb
@@ -3,7 +3,7 @@ require "#{BOT_ROOT}/vendor/escape/escape"
 class Fun < CampfireBot::Plugin
   on_command    'figlet', :figlet
   
-  def figlet(m)
+  def figlet(msg)
     output = `#{Escape.shell_command(['figlet', '--', m[:message]])}`
     msg.paste output
   end
