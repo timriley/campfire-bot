2b7cb3fb0e6c427e2a36da08c4d3f811d1c4c065
joshwand <jwand@josh-wand.local>
2009-01-22 19:35:53 -0500

    more work on beer and specs

--- spec/beer_spec.rb
+++ spec/beer_spec.rb
@@ -1,6 +1,6 @@
 require 'spec'
 BOT_ROOT        = File.join(File.dirname(__FILE__), '..')
-BOT_ENVIRONMENT = 'test'
+BOT_ENVIRONMENT = 'development'
 
 require File.join(File.dirname(__FILE__), '../lib/bot.rb')
 
@@ -8,29 +8,67 @@ describe "giving beer" do
   before(:all) do
     bot = CampfireBot::Bot.instance
     # bot.config = []
-    bot.stub!('config').and_return({'nickname' => 'foo'})
+    # bot.stub!(:config).and_return({'nickname' => 'foo'})
+    # bot.send(:load_plugins)
     require "#{BOT_ROOT}/plugins/beer.rb"
-    @beer = Beer.new() 
+    CampfireBot::Plugin.registered_plugins['Beer'] = CampfireBot::Plugin::Beer.new()
+    @beer = CampfireBot::Plugin.registered_plugins['Beer']
   end
   
   it "should respond to the command !give_beer" do
-    @message = mock('message')
-    @message.stub!(:[]).with(:person).and_return('Josh')
-    @message.stub!(:[]).with(:message).and_return('!give_beer Bot')
-    @message.should_receive(:speak)
-    @beer.give_beer(@message)
+
+    @message = CampfireBot::Message.new(:person => 'Josh', :message => '!give_beer')
+    @beer.should_receive(:give_beer)
+    bot.send(:handle_message, @message)
+  
   end
   
-  it "should increase my balance with Bot" do
-    @message = mock('message')
-    @message.stub!(:[]).with(:message).and_return('!give_beer Foo')
-    @message.stub!(:[]).with(:person).and_return('Josh')
-    @message.should_receive(:speak)
+  it "should increase my balance with Foo" do
+    @message = CampfireBot::Message.new(:person => 'Josh', :message => '!give_beer Foo')
     
     bal = @beer.balance('Josh', 'Foo')
-    @beer.give_beer(@message)
+    # @beer.should_receive(:give_beer)
+    # @beer.should_receive(:beer_transaction)
+    @message.stub!(:speak)
+    
+    p "initial bal is #{bal}"
+    bot.send(:handle_message, @message)
+    p 'sent message'
+    
     @beer.balance('Josh', 'Foo').should eql(bal - 1)
+  end
+  
+  it "should say back to me what my balance is" do
+    @message = CampfireBot::Message.new(:person => 'Josh', :message => '!give_beer Foo')
+    @message.should_receive(:speak)
     
   end
   
 end
+
+describe "demanding beer" do
+  it "should respond to the command !demand_beer" do
+        pending
+  end
+  
+  it "should decrease my balance with the opposite party" do
+        pending
+  end
+  
+end
+
+describe "redeeming beer" do
+  it "should respond to the command !redeem_beer" do
+    pending
+  end
+  
+  it "should increase my balance with the opposite party" do
+    pending    
+  end
+  
+  it "should not increase my balance if it is already zero" do
+        pending
+  end
+end
+
+
