f088973994e28753923bff6603034b1dada2a041
joshwand <jwand@josh-wand.local>
2009-02-03 06:01:30 +0800

    infobot should use new plugin structure
    Signed-off-by: Tim Riley <tim@openmonkey.com>

--- plugins/infobot.rb
+++ plugins/infobot.rb
@@ -23,10 +23,10 @@ class Infobot < CampfireBot::Plugin
     puts msg[:message] =~ RESPOND_REGEXP # Regexp.new("^#{Bot.instance.config['nickname']},\\s+#{RESPOND_REGEXP.source}", Regexp::IGNORECASE)
     puts $1, $2, $3
     if !@facts.has_key?($2.downcase)
-      speak("Sorry, I don't know what #{$2} is.")
+      msg.speak("Sorry, I don't know what #{$2} is.")
     else
       fact = @facts[$2.downcase]
-      speak("#{msg[:person].split(" ")[0]}, #{$2} is #{fact}.")
+      msg.speak("#{msg[:person].split(" ")[0]}, #{$2} is #{fact}.")
     end
   end
   
@@ -35,11 +35,11 @@ class Infobot < CampfireBot::Plugin
     @facts = init()
     # puts @facts
     # puts msg[:message]
-    # puts msg[:message] =~ Regexp.new("^#{bot.config['nickname']},\\s+#{DEFINE_REGEXP.source}", Regexp::IGNORECASE)
+    puts msg[:message] =~ Regexp.new("^#{bot.config['nickname']},\\s+#{DEFINE_REGEXP.source}", Regexp::IGNORECASE)
     # puts @define_regexp
     # puts $1, $2, $3
     @facts[$2.downcase] = $3
-    speak("Okay, #{$2} is now #{$3}")
+    msg.speak("Okay, #{$2} is now #{$3}")
     File.open(File.join(File.dirname(__FILE__), 'infobot.yml'), 'w') do |out|
       YAML.dump(@facts, out)
     end
@@ -52,7 +52,7 @@ class Infobot < CampfireBot::Plugin
   
   def reload(msg)
     @facts = init()
-    speak("ok, reloaded #{@facts.size} facts")
+    msg.speak("ok, reloaded #{@facts.size} facts")
   end
   
 end
