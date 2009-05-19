e90fa7db4cf4aaf3dcbec2da5f721620c196ee02
Tim Riley <tim@openmonkey.com>
2009-05-20 09:35:14 +1000

    Revert "html entities decoding in unfuddle plugin"
    This reverts commit f542d25d1e4cff4a4cf844fbd7ce817d4add766f.

--- plugins/unfuddle.rb
+++ plugins/unfuddle.rb
@@ -1,7 +1,18 @@
+# Unfuddle RSS echo plugin. Uses the following config.yml settings:
+#
+# unfuddle:
+#   domain: you.unfuddle.com
+#   rss_path: "/account/activity.rss?aak=aaaaaaaaaaaaabbbbbbbbccccccccddddddeeee0000"
+#   port: 443
+#   msg_filters:
+#     - reassigned
+#     - closed
+#     - commented
+#     - resolved
+
 require 'rubygems'
 require 'simple-rss'
 require 'net/https'
-require 'htmlentities'
 
 class Unfuddle < CampfireBot::Plugin
   at_interval 2.minutes, :fetch_rss
@@ -9,23 +20,50 @@ class Unfuddle < CampfireBot::Plugin
 
   def initialize
     @last_item = 12.hours.ago
-    @http = Net::HTTP.new(bot.config['unfuddle_domain'], bot.config['unfuddle_port'])
+    @http = Net::HTTP.new(bot.config['unfuddle']['domain'], bot.config['unfuddle']['port'])
 
-    if bot.config['unfuddle_port'] == 443
+    if bot.config['unfuddle']['port'] == 443
       @http.use_ssl = true
       @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
   end
 
   def fetch_rss(msg)
-    feed = SimpleRSS.parse(@http.get(bot.config['unfuddle_rss_path']).body)
+    feed = SimpleRSS.parse(@http.get(bot.config['unfuddle']['rss_path']).body)
 
     feed.items.each do |item|
-      msg.speak HTMLEntities.new.decode("#{item.title} #{item.link}") if item.pubDate > @last_item
+      msg.speak "#{item.title} #{item.link}" if published?(item) && !filtered?(item.title)
     end
 
     @last_item = feed.items.first.pubDate
   rescue => e
     msg.speak e
+  end
+
+  def published?(item)
+    item.pubDate > @last_item
+  end
+
+  def filtered?(title)
+    bot.config['unfuddle']['msg_filters'].each do |msg_filter|
+      return true if send("#{msg_filter}?", title)
+    end
+    return false
+  end
+
+  def closed?(title)
+    (title =~ /Closed Ticket:/) != nil
+  end
+
+  def reassigned?(title)
+    (title =~ /Reassigned Ticket:/) != nil
+  end
+
+  def commented?(title)
+    (title =~ /Created Ticket Comment:/) != nil
+  end
+
+  def resolved?(title)
+    (title =~ /Resolved Ticket:/) != nil
   end
 end
