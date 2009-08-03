3affc7b7c11e20665aa4e57e5b9f1d54ca582129
Tim Riley <tim@openmonkey.com>
2009-08-03 12:50:28 +1000

    syntax fix

--- plugins/accountability.rb
+++ plugins/accountability.rb
@@ -17,7 +17,7 @@ class Accountability < CampfireBot::Plugin
     now = Time.now
     
     # Only do this once a day, sometime between 11:00 and 11:05.
-    if (Time.mktime(now.year, now.month, now.day, 11, 0)..Time.mktime(now.year, now.month, now.day, 11, 5).include?(now)
+    if (Time.mktime(now.year, now.month, now.day, 11, 0)..Time.mktime(now.year, now.month, now.day, 11, 5)).include?(now)
       (@room.users - @informative_people).each do |person|
         @room.speak("#{person}: We haven't seen your INDAY today. So what's the plan?")
       end
