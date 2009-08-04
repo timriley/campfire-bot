3f94bb6ded34dccd82ea347c82a4b96e237dc0c9
Tim Riley <tim@openmonkey.com>
2009-08-04 14:10:25 +1000

    fixes to make Accountability work properly.

--- plugins/accountability.rb
+++ plugins/accountability.rb
@@ -3,26 +3,43 @@ class Accountability < CampfireBot::Plugin
   at_interval 5.minutes, :keep_them_honest
   
   def initialize
-    @informative_people = []
-    @room = bot.rooms[bot.config['accountability_room']]
+    @accountable_people = []
   end
   
   def save_speaker(msg)
-    if msg[:room] == bot.config['accountability_room']
-      @informative_people << msg[:person] unless @informative_people.include?(msg[:person])
+    if msg[:room].name == accountability_room.name
+      puts "WE'RE IN!"
+      @accountable_people << strip_tags(msg[:person]) unless @accountable_people.include?(strip_tags(msg[:person]))
     end
   end
   
   def keep_them_honest(msg)
-    now = Time.now
-    
-    # Only do this once a day, sometime between 11:00 and 11:05.
-    if (Time.mktime(now.year, now.month, now.day, 11, 0)..Time.mktime(now.year, now.month, now.day, 11, 5)).include?(now)
-      (@room.users - @informative_people).each do |person|
+    if accountability_time?
+      unaccountable_people.each do |person|
         @room.speak("#{person}: We haven't seen your INDAY today. So what's the plan?")
       end
       
-      @informative_people.clear
+      @accountable_people.clear
     end
+  end
+  
+  private
+  
+  def unaccountable_people
+    accountability_room.users.map { |u| strip_tags(u) } - @accountable_people - [bot.config['nickname']]
+  end
+  
+  def accountability_room
+    @room ||= bot.rooms[bot.config['accountability_room']] || bot.rooms.first
+  end
+  
+  def accountability_time?
+    # Once a day, sometime between 11:00 and 11:05.
+    now = Time.now
+    (Time.mktime(now.year, now.month, now.day, 11, 0)..Time.mktime(now.year, now.month, now.day, 11, 5)).include?(now)
+  end
+  
+  def strip_tags(str)
+    str.gsub(/<\/?[^>]*>/, '').strip
   end
 end
